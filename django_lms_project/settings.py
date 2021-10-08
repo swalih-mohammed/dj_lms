4c8185ad6c239f2b62573457526c2c109705a1b2
sibiyan <sibiyan9030@gmail.com>
2021-10-08 22:07:30 +0530

    added static root

--- django_lms_project/settings.py
+++ django_lms_project/settings.py
@@ -141,6 +141,9 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = "/home/myusername/myproject/stati
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
