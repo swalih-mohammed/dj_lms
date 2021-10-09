9f820c3ace322afd575dc46472f47224b93863e7
sibiyan <sibiyan9030@gmail.com>
2021-10-09 18:44:18 +0530

    changed course

--- courses/models.py
+++ courses/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
 
+class Language(models.Model):
+    title = models.CharField(max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'languages'
+
+
 class Course(models.Model):
     title = models.CharField(max_length=250, blank=True, null=True)
     subtitle = models.CharField(max_length=250, blank=True, null=True)
+    description = models.CharField(max_length=250, blank=True, null=True)
+    language = models.ForeignKey(
+        Language, related_name='courses', blank=True, null=True, max_length=250, on_delete=models.CASCADE)
+    is_for_nursary = models.BooleanField(default=False)
+    is_free = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    has_units = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
