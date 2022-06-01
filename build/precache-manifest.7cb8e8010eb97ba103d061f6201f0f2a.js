dd8923b3a7a80149fa973669a8a2d24aedf566c6
sibiyan <sibiyan9030@gmail.com>
2022-06-02 00:47:03 +0530

    build

---
+++ build/precache-manifest.7cb8e8010eb97ba103d061f6201f0f2a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d50c4887681ee0fbc00177417443fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3255e6f8216c013850ab",
+    "url": "./static/js/2.e4bca893.chunk.js"
+  },
+  {
+    "revision": "9b9de423b52b798b8fc649e7d9e276fb",
+    "url": "./static/js/2.e4bca893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63bd991b06468093a48",
+    "url": "./static/js/main.609de7ba.chunk.js"
+  },
+  {
+    "revision": "4e681116cd9089599049",
+    "url": "./static/js/runtime-main.9904f276.js"
+  },
+  {
+    "revision": "61ee6d42c38c159f8f2351ab53bb9a21",
+    "url": "./static/media/application.61ee6d42.png"
+  },
+  {
+    "revision": "ce29a28f60d801c52efa196f26e5968d",
+    "url": "./static/media/laam2.ce29a28f.png"
+  },
+  {
+    "revision": "52ea952446b8f794d1ef26fbbef098c8",
+    "url": "./static/media/section_1.52ea9524.png"
+  },
+  {
+    "revision": "5c42ff657d836981f55f4aabbb923168",
+    "url": "./static/media/section_4.5c42ff65.jpg"
+  },
+  {
+    "revision": "827027ddbc4444cf9e4cd89e9438825d",
+    "url": "./static/media/section_5.827027dd.jpg"
+  }
+]);
