3e9734086047effc6ce1422c6590353c71505527
Swalih  Mohammed <swalihmohammed@Swalihs-MacBook-Air.local>
2022-06-26 18:31:30 +0530

    front end

--- laam-web/src/App.js
+++ laam-web/src/App.js
@@ -1,39 +1,65 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import GlobalStyle from "./globalStyles";
-import Home from "./pages/HomePage/Home";
-import About from "./pages/About/About";
-import Support from "./pages/Support/Support";
-import PasswordReset from "./pages/PasswordReset/PasswordReset";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
-import { Navbar, Footer } from "./components";
-import Privacy from "./pages/Privacy/privacy";
+import Home from "./Pages/Home/index";
+import AppBar from '@mui/material/AppBar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import MenuSharp from '@mui/icons-material/MenuSharp';
+import Navbar from "./Components/Navbar"
+
+
+// import About from "./pages/About/About";
+// import Support from "./pages/Support/Support";
+// import PasswordReset from "./pages/PasswordReset/PasswordReset";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+// import ScrollToTop from "./components/ScrollToTop";
+import Footer  from "./Components/Footer";
+// import Privacy from ". /pages/Privacy/privacy";
 
 function App() {
   return (
-    <Router>
-      <GlobalStyle />
-      <ScrollToTop />
+    <BrowserRouter>
+      {/* <GlobalStyle /> */}
+      {/* <ScrollToTop /> */}
       <Helmet>
         <meta charSet="utf-8" />
         <title>Laam Academy | Learn English online</title>
         <meta name="description" content="Learn English from Laam Academy" />
         <link rel="canonical" href="http://laamacademy.com" />
       </Helmet>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/support" component={Support} />
-        <Route
+      {/* <Navbar /> */}
+      <CssBaseline />
+      {/* <AppBar position="relative">
+        <Toolbar>
+          <MenuSharp sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Laam Academy
+          </Typography>
+        </Toolbar>
+      </AppBar> */}
+      <Navbar/>
+    
+    <Routes>
+        <Route path="/"  element={<Home/>} />
+        {/* <Route path="/about" component={About} /> */}
+        {/* <Route path="/support" component={Support} /> */}
+        {/* <Route
           path="/dj-rest-auth/password/reset/confirm/:id/:token"
           component={PasswordReset}
-        />
-        <Route path="/privacy-policy" exact component={Privacy} />
-      </Switch>
+        /> */}
+        {/* <Route path="/privacy-policy" exact component={Privacy} /> */}
+        </Routes>
+ 
       <Footer />
-    </Router>
+      </BrowserRouter>
   );
 }
 
--- laam-web/src/Pages/Home/Index.js
+++ laam-web/src/Pages/Home/Index.js
@@ -1,24 +1,23 @@
 import React from "react";
-// import SectionOne from "./Section_1";
-// import SectionTwo from "./Section_2";
-// import SectionThree from "./Section_3";
-// import SectionFour from "./Section_4";
-// import SectionFive from "./Section_5";
+import SectionOne from "./Section_1";
+import SectionTwo from "./Section_2";
+import SectionThree from "./Section_3";
+import SectionFour from "./Section_4";
+import SectionFive from "./Section_5";
 
 function Home() {
-  console.log("1234");
+
   React.useEffect(() => {
-    // console.log(props.match.params.id);
-    console.log("testing from support");
+    console.log("Home");
   }, []);
   return (
     <>
-    <div
-      // <SectionOne />
-      // <SectionTwo />
-      // <SectionFour />
-      // <SectionThree />
-      // <SectionFive />
+<SectionOne/>
+<SectionTwo/>
+<SectionThree/>
+<SectionFour/>
+<SectionFive/>
+
     </>
   );
 }
