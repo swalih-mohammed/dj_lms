2fcd8b89f6b25c13b1fea8c2a8d8d597d3378bb4
Swalih  Mohammed <swalihmohammed@Swalihs-MacBook-Air.local>
2022-06-26 20:26:59 +0530

    front end

--- django_lms_project/settings.py
+++ django_lms_project/settings.py
@@ -174,7 +174,7 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'laam-web/build/static')]
 # STATIC_ROOT = "/home/myusername/myproject/stati
 
